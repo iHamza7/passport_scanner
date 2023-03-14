ca26483c168f279a2113020ee23e7925368331b3
iHamza7 <khanazam3324@gmail.com>
2023-03-13 20:38:13 -0700

    changed

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:permission_handler/permission_handler.dart';
+
+import 'camerpage.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FutureBuilder<PermissionStatus>(
+        future: Permission.camera.request(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data == PermissionStatus.granted) {
+            return const CameraPage();
+          }
+          if (snapshot.data == PermissionStatus.permanentlyDenied) {
+            // The user opted to never again see the permission request dialog for this
+            // app. The only way to change the permission's status now is to let the
+            // user manually enable it in the system settings.
+            openAppSettings();
+          }
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CircularProgressIndicator(),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Awaiting for permissions'),
+                  ),
+                  Text('Current status: ${snapshot.data?.toString()}'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
